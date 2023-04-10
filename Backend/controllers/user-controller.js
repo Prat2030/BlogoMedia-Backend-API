24f4e9a219d087b53b9cd87d4a297c26d7cb02c7
Prat2030 <pratyushjindal2003@gmail.com>
2023-04-11 02:20:30 +0530

    login added

--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../model/User";
+import bcrypt from "bcryptjs";
 
 export const getAllUser = async (req, res, next) => {
     let users;
@@ -24,10 +25,11 @@ export const signup = async (req, res, next) => {
     if(existingUser) {
         return res.status(422).json({message: "User already exists"});
     }
+    const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({
         name,
         email,
-        password
+        password: hashedPassword
     });
 
     try {
@@ -37,4 +39,22 @@ export const signup = async (req, res, next) => {
         return console.log(err);
     }
     return res.status(201).json({newUser, message: "User created"});
+}
+
+export const login = async (req, res, next) => {
+    const {email, password} = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!existingUser) {
+        return res.status(404).json({message: "User not found"});
+    }
+    let isValidPassword = await bcrypt.compareSync(password, existingUser.password);
+    if(!isValidPassword) {
+        return res.status(401).json({message: "Incorrect Password"});
+    }
+    return res.status(200).json({message: "Login Successful"});
 }
